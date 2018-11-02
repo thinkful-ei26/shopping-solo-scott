75ebc9eff168cd880a0efe0605cd80e329d4efd6
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-02 08:49:42 -0700

    completed rendering function

--- index.js
+++ index.js
@@ -8,23 +8,35 @@ const STORE = [
   {name: 'bread', checked: false}
 ];
 
+function generateItemElement(item, index, template){
+  return `
+  <li class="js-item-index-element" 
+  data-item-index="${item.index}">
+    <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+}
+
+
 function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
+  console.log('Generating shopping list element');
   const items = shoppingList.map((item, index) => generateItemElement(item, index));
 
 
-  return `
-    <li>apples</li>
-    <li>oranges</li>
-    <li>milk</li>
-    <li>bread</li>
-  `;
+  return items.join('');
 }
 
 function renderShoppingList() {
   console.log('`renderShoppingList`');
   const shoppingListItemsString = generateShoppingItemsString(STORE);
-  const shoppingListItemsString = '<li>apples</li>';
+  //const shoppingListItemsString = '<li>apples</li>';
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
 }
@@ -43,7 +55,7 @@ function handleItemCheckClicked() {
 function handleDeleteItemClicked() {
   // this function will be responsible for when users want to delete a shopping list
   // item
-  console.log('`handleDeleteItemClicked` ran')
+  console.log('`handleDeleteItemClicked` ran');
 }
 
 //doc ready function
